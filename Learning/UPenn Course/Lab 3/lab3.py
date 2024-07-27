07242335c10e3596654d0a238ecffd9ca4f32277
Ishaan <ishaanpaul@hotmail.com>
2024-07-26 17:32:40 -0700

    Working on lab 3

--- Learning/UPenn Course/Lab 3/lab3.py
+++ Learning/UPenn Course/Lab 3/lab3.py
@@ -3,7 +3,7 @@ import os
 import zipfile # To handle zip files
 import torch as torch
 
-def load_movie(movie, minRatings):
+def load_data(movie, minRatings):
 
     dataDir = os.getcwd()
     zipObject = zipfile.ZipFile(os.path.join(dataDir, 'ml-100k.zip'))
@@ -46,4 +46,94 @@ def load_movie(movie, minRatings):
     
     return X, idxMovie
 
+def create_graph(X, idxTrain, knn):
 
+    zeroTolerance = 1e-9
+
+    N = X.shape[1]
+
+    XTrain = np.transpose(X[idxTrain, :])
+
+    binaryTemplate = (XTrain > 0).astype(XTrain.dtype)
+    sumMatrix = XTrain.dot(binaryTemplate.T)
+    countMatrix = binaryTemplate.dot(binaryTemplate.T)
+    countMatrix[countMatrix == 0] = 1
+    avgMatrix = sumMatrix/countMatrix
+    sqSumMatrix = XTrain.dot(XTrain.T)
+    correlationMatrix = sqSumMatrix / countMatrix - avgMatrix * avgMatrix.T
+
+    sqrtDiagonal = np.sqrt(np.diag(correlationMatrix))
+    nonzeroSqrtDiagonalIndex = (sqrtDiagonal > zeroTolerance).astype(sqrtDiagonal.dtype)
+    sqrtDiagonal[sqrtDiagonal < zeroTolerance] = 1
+    invSqrtDiagonal = 1/sqrtDiagonal
+    invSqrtDiagonal = invSqrtDiagonal * nonzeroSqrtDiagonalIndex
+    normalizationMatrix = np.diag(invSqrtDiagonal)
+
+    normalizedMatrix = normalizationMatrix.dot(correlationMatrix.dot(normalizationMatrix)) - np.eye(correlationMatrix.shape[0])
+
+    normalizationMatrix[np.abs(normalizedMatrix) < zeroTolerance] = 0
+    W = normalizedMatrix
+
+    WSorted = np.sort(W, axis=1)
+    threshold = WSorted[:, -knn].squeeze()
+    thresholdMatrix = (np.tile(threshold, (N, 1))).transpose()
+    W[W<thresholdMatrix] = 0
+
+    E, V = np.linalg.eig(W)
+    W = W/np.max(np.abs(E))
+
+    return W
+
+
+
+def split_data(X, idxTrain, idxTest, idxMovie):  
+    
+    N = X.shape[1]
+    
+    xTrain = X[idxTrain,:]
+    idx = np.argwhere(xTrain[:,idxMovie]>0).squeeze()
+    xTrain = xTrain[idx,:]
+    yTrain = np.zeros(xTrain.shape)
+    yTrain[:,idxMovie] = xTrain[:,idxMovie]
+    xTrain[:,idxMovie] = 0
+    
+    xTrain = torch.tensor(xTrain)
+    xTrain = xTrain.reshape([-1,1,N])
+    yTrain = torch.tensor(yTrain)
+    yTrain = yTrain.reshape([-1,1,N])
+    
+    xTest = X[idxTest,:]
+    idx = np.argwhere(xTest[:,idxMovie]>0).squeeze()
+    xTest = xTest[idx,:]
+    yTest = np.zeros(xTest.shape)
+    yTest[:,idxMovie] = xTest[:,idxMovie]
+    xTest[:,idxMovie] = 0
+    
+    xTest = torch.tensor(xTest)
+    xTest = xTest.reshape([-1,1,N])
+    yTest = torch.tensor(yTest)
+    yTest = yTest.reshape([-1,1,N])
+    
+    return xTrain, yTrain, xTest, yTest
+
+def movieMSELoss(yHat,y,idxMovie):
+    mse = torch.nn.MSELoss()
+    return mse(yHat[:,:,idxMovie].reshape([-1,1]),y[:,:,idxMovie].reshape([-1,1]))
+
+def main():
+
+    X, idxContact = load_data(movie=257, min_ratings=150)
+
+    # Creating and sparsifying the graph
+
+    nTotal = X.shape[0] # total number of users (samples)
+    permutation = np.random.permutation(nTotal)
+    nTrain = int(np.ceil(0.9*nTotal)) # number of training samples
+    idxTrain = permutation[0:nTrain] # indices of training samples
+    nTest = nTotal-nTrain # number of test samples
+    idxTest=permutation[nTrain:nTotal] # indices of test samples
+
+    W = create_graph(X=X, idxTrain=idxTrain, knn=40)
+    xTrain, yTrain, xTest, yTest = split_data(X, idxTrain, idxTest, idxContact)
+    nTrain = xTrain.shape[0]
+    nTest = xTest.shape[0]
