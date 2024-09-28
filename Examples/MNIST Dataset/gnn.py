a1fde22e3b940dcdeb72e99789fa7e17b1b9edec
Ishaan <ishaanpaul@hotmail.com>
2024-09-28 14:50:23 -0700

    Completed Q1
    Completed Q1 in Lab 1

--- Examples/MNIST Dataset/gnn.py
+++ Examples/MNIST Dataset/gnn.py
@@ -17,3 +17,29 @@ testset = torchvision.datasets.MNIST(root = "./data", train = False, download = 
 
 testloader = torch.utils.data.DataLoader(testset, batch_size = BATCH_SIZE, shuffle = False, num_workers = 2)
 
+class MyModel(nn.Module):
+    def __init__(self):
+        super(MyModel, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 32, kernel_size = 3)
+        self.d1 = nn.Linear(26 * 26 * 32, 128)
+        self.d2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        # 32x1x28x28 -> 32x32x26x26
+        x = self.conv1(x)
+        x = F.relu(x)
+        
+        #Flatten -> 32 x (32x26x26)
+        x = x.flatten(start_dim = 1)
+
+        #32 x (32x26x26) -> 32*128
+        x = self.d1(x)
+        x = F.relu(x)
+
+        #logits -> 32, 10
+        logits = self.d2(x)
+        out = F.softmax(logits, dim = 1)
+        return out
+    
+
