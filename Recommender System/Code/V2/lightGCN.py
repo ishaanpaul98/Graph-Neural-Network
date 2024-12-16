26a4f7657bea371bcc18839ef1c219e45afd827e
Ishaan <ishaanpaul@hotmail.com>
2024-12-16 09:39:45 +0530

    Completed py file conversion

--- Recommender System/Code/V2/lightGCN.py
+++ Recommender System/Code/V2/lightGCN.py
@@ -1,19 +1,18 @@
 import numpy as np
 import torch
 import torch.nn.functional as F
-from torch import nn, optim, Tensor
+from torch import nn, optim
 
 from torch_geometric.utils import structured_negative_sampling
 from torch_geometric.nn.conv.gcn_conv import gcn_norm
 from torch_geometric.nn import LGConv
 
-from sentence_transformers import SentenceTransformer
-from torch_geometric.data import HeteroData
-from torch_geometric.transforms import RandomLinkSplit, ToUndirected
-from sklearn.model_selection import train_test_split
 import pandas as pd
 import torch
-import sentence_transformers
+
+K = 20
+LAMBDA = 1e-6
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 class LightGCN(nn.Module):
@@ -47,5 +46,163 @@ class LightGCN(nn.Module):
         emb_final = 1/(self.num_layers+1) * torch.mean(torch.stack(embs, dim=1), dim=1)
         #Splitting emb_final
         emb_users_final, emb_items_final = torch.split(emb_final, [self.num_users, self.num_items])
-        
+
         return emb_users_final, self.emb_users.weight, emb_items_final, self.emb_items.weight
+    
+
+def bpr_loss(emb_users_final, emb_users, emb_pos_items_final, emb_pos_items, emb_neg_items_final, emb_neg_items):
+    reg_loss = LAMBDA * (emb_users.norm().pow(2) +
+                        emb_pos_items.norm().pow(2) +
+                        emb_neg_items.norm().pow(2))
+
+    pos_ratings = torch.mul(emb_users_final, emb_pos_items_final).sum(dim=-1)
+    neg_ratings = torch.mul(emb_users_final, emb_neg_items_final).sum(dim=-1)
+
+    bpr_loss = torch.mean(torch.nn.functional.softplus(pos_ratings - neg_ratings))
+    # bpr_loss = torch.mean(torch.nn.functional.logsigmoid(pos_ratings - neg_ratings))
+
+    return -bpr_loss + reg_loss
+
+def sample_mini_batch(edge_index, BATCH_SIZE = 1024):
+    # Generate BATCH_SIZE random indices
+    index = np.random.choice(range(edge_index.shape[1]), size=BATCH_SIZE)
+
+    # Generate negative sample indices
+    edge_index = structured_negative_sampling(edge_index)
+    edge_index = torch.stack(edge_index, dim=0)
+    
+    user_index = edge_index[0, index]
+    pos_item_index = edge_index[1, index]
+    neg_item_index = edge_index[2, index]
+    
+    return user_index, pos_item_index, neg_item_index
+
+def get_user_items(edge_index):
+    user_items = dict()
+    for i in range(edge_index.shape[1]):
+        user = edge_index[0][i].item()
+        item = edge_index[1][i].item()
+        if user not in user_items:
+            user_items[user] = []
+        user_items[user].append(item)
+    return user_items
+
+def compute_recall_at_k(items_ground_truth, items_predicted):
+    num_correct_pred = np.sum(items_predicted, axis=1)
+    num_total_pred = np.array([len(items_ground_truth[i]) for i in range(len(items_ground_truth))])
+
+    recall = np.mean(num_correct_pred / num_total_pred)
+
+    return recall
+
+def compute_ndcg_at_k(items_ground_truth, items_predicted):
+    test_matrix = np.zeros((len(items_predicted), K))
+
+    for i, items in enumerate(items_ground_truth):
+        length = min(len(items), K)
+        test_matrix[i, :length] = 1
+    
+    max_r = test_matrix
+    idcg = np.sum(max_r * 1. / np.log2(np.arange(2, K + 2)), axis=1)
+    dcg = items_predicted * (1. / np.log2(np.arange(2, K + 2)))
+    dcg = np.sum(dcg, axis=1)
+    idcg[idcg == 0.] = 1.
+    ndcg = dcg / idcg
+    ndcg[np.isnan(ndcg)] = 0.
+    
+    return np.mean(ndcg)
+
+def get_metrics(model, edge_index, exclude_edge_indices):
+
+    ratings = torch.matmul(model.emb_users.weight, model.emb_items.weight.T)
+
+    for exclude_edge_index in exclude_edge_indices:
+        user_pos_items = get_user_items(exclude_edge_index)
+        exclude_users = []
+        exclude_items = []
+        for user, items in user_pos_items.items():
+            exclude_users.extend([user] * len(items))
+            exclude_items.extend(items)
+        ratings[exclude_users, exclude_items] = -1024
+
+    # get the top k recommended items for each user
+    _, top_K_items = torch.topk(ratings, k=K)
+
+    # get all unique users in evaluated split
+    users = edge_index[0].unique()
+
+    test_user_pos_items = get_user_items(edge_index)
+
+    # convert test user pos items dictionary into a list
+    test_user_pos_items_list = [test_user_pos_items[user.item()] for user in users]
+
+    # determine the correctness of topk predictions
+    items_predicted = []
+    for user in users:
+        ground_truth_items = test_user_pos_items[user.item()]
+        label = list(map(lambda x: x in ground_truth_items, top_K_items[user]))
+        items_predicted.append(label)
+
+    recall = compute_recall_at_k(test_user_pos_items_list, items_predicted)
+    ndcg = compute_ndcg_at_k(test_user_pos_items_list, items_predicted)
+
+    return recall, ndcg
+
+def test(model, edge_index, exclude_edge_indices):
+    emb_users_final, emb_users, emb_items_final, emb_items = model.forward(edge_index)
+    user_indices, pos_item_indices, neg_item_indices = structured_negative_sampling(edge_index, contains_neg_self_loops=False)
+
+    emb_users_final, emb_users = emb_users_final[user_indices], emb_users[user_indices]
+
+    emb_pos_items_final, emb_pos_items = emb_items_final[pos_item_indices], emb_items[pos_item_indices]
+    emb_neg_items_final, emb_neg_items = emb_items_final[neg_item_indices], emb_items[neg_item_indices]
+
+    loss = bpr_loss(emb_users_final, emb_users, emb_pos_items_final, emb_pos_items, emb_neg_items_final, emb_neg_items).item()
+
+    recall, ndcg = get_metrics(model, edge_index, exclude_edge_indices)
+
+    return loss, recall, ndcg
+
+def training(num_users, num_items, train_index, BATCH_SIZE = 1024):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model = LightGCN(num_users, num_items)
+    model = model.to(device)
+    edge_index = edge_index.to(device)
+    train_edge_index = train_edge_index.to(device)
+    val_edge_index = val_edge_index.to(device)
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    n_batch = int(len(train_index)/BATCH_SIZE)
+
+    for epoch in range(31):
+        model.train()
+
+    for _ in range(n_batch):
+        optimizer.zero_grad()
+
+        emb_users_final, emb_users, emb_items_final, emb_items = model.forward(train_edge_index)
+
+        user_indices, pos_item_indices, neg_item_indices = sample_mini_batch(train_edge_index)
+        
+        emb_users_final, emb_users = emb_users_final[user_indices], emb_users[user_indices]
+        emb_pos_items_final, emb_pos_items = emb_items_final[pos_item_indices], emb_items[pos_item_indices]
+        emb_neg_items_final, emb_neg_items = emb_items_final[neg_item_indices], emb_items[neg_item_indices]
+
+        train_loss = bpr_loss(emb_users_final, emb_users, emb_pos_items_final, emb_pos_items, emb_neg_items_final, emb_neg_items)
+
+        train_loss.backward()
+        optimizer.step()
+
+    if epoch % 5 == 0:
+        model.eval()
+        val_loss, recall, ndcg = test(model, val_edge_index, [train_edge_index])
+        print(f"Epoch {epoch} | Train loss: {train_loss.item():.5f} | Val loss: {val_loss:.5f} | Val recall@{K}: {recall:.5f} | Val ndcg@{K}: {ndcg:.5f}")
+
+
+def main():
+    training()
+
+if __name__ == "__main__":
+    main()
